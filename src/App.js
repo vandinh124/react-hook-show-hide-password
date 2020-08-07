189773d499ebfb4e6548a6c593489df5366ed5e3
vandinh124 <vanvdinh124@gmail.com>
2020-08-06 20:41:23 -0700

    initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState} from 'react'
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [showPassword, setShowPassword] = useState(true);
+
+    return (
+        <div className="container">
+            <input
+                type='text'
+                placeholder='Enter username'
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+            />
+            <input
+                // type='password'
+                //Show / hide password in the input
+                type={showPassword ? 'text' : 'password'}
+                placeholder='Enter password'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <h1>{username}</h1>
+            {/* If the showPassword is true then we display Show else
+             we display * sign as man times as there are letters in the password */}
+            <h1>{showPassword ? password : '*'.repeat(password.length)}</h1>
+            <button onClick={(e) => {setShowPassword(!showPassword)}}>Show/Hide password</button>
+        </div>
+    )
 }
 
 export default App;
